bca809fb67e0af5f9efb6fa54f3b31e4f23ba4b9
Roy Kollen Svendsen <roykollensvendsen@gmail.com>
2017-06-05 07:43:57 +0000

    first commit

---
+++ setup-skivevender.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install git
+git clone https://github.com/roykollensvendsen/skive_vender.git
+sudo apt install nodejs
+sudo apt install npm
+sudo apt install python-pifacedigitalio
+
+sudo sed -i s/#dtparam=spi=on/dtparam=spi=on/g /boot/config.txt
+
+cd skrive_vender
+npm install
+
+
+sudo apt install dnsmasq hostapd
+
+echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
+
+sudo cp interfaces /etc/network/interfaces
+
+sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+
+sudo cp dnsmasq.conf /etc/dnsmasq.conf
+
+sudo cp iptables.ipv4.nat /etc/iptables.ipv4.nat
+
+sudo cp local.rc /etc/local.rc
+
+sudo reboot -h now
+
